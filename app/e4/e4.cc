106f121ae2d16d2d8c969595785665d25238a285
Luiz Motta <luizjoaomotta@gmail.com>
2022-09-20 20:16:27 -0300

    Add e4 exercice

---
+++ app/e4/e4.cc
@@ -0,0 +1,37 @@
+#include <utility/ostream.h>
+#include <process.h>
+
+using namespace EPOS;
+
+OStream cout;
+
+const unsigned int thread_num = 5;
+Thread * t[thread_num];
+int code;
+
+int func_a() {
+  cout << code-- << " " << endl;
+  return 0;
+}
+
+int func_b() {
+  for (unsigned int i = 0; i < thread_num; ++i) {
+    t[i] = new Thread(Thread::Configuration(Thread::READY, Thread::HIGH), &func_a);
+    cout << code++ << " " << endl;
+  }
+  for (unsigned int i = 0; i < thread_num; ++i) {
+    t[i]->join();
+    delete t[i];
+  }
+  return 0;
+}
+
+int main()
+{
+  cout << "Simple Test" << endl;
+  Thread * c = new Thread(Thread::Configuration(Thread::RUNNING, Thread::NORMAL -1), &func_b);
+  c->join();
+  cout << "\nThe end!" << endl;
+  delete c;
+  return 0;
+}
