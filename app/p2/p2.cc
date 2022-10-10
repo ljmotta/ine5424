5b9130c139bebbdd51d1aa24619dc81c5dc30296
Luiz Motta <luizjoaomotta@gmail.com>
2022-10-10 01:01:58 -0300

    Fix p2

--- app/p2/p2.cc
+++ app/p2/p2.cc
@@ -11,13 +11,10 @@ int main()
 {
     cout << "Hello World" << endl;
 
-    unsigned * data_base = reinterpret_cast<unsigned*>(Memory_Map::APP_DATA);
-    Segment * code_seg1 = new Segment(1024*4, MMU::Flags::SYS);
-    Segment * data_seg1 = new Segment(1024*4, MMU::Flags::SYS);
-    cout << "Create Task1" << endl;
+    unsigned long *data_base = reinterpret_cast<unsigned long*>(Memory_Map::APP_DATA);
+    Segment *code_seg1 = new Segment(1024*4, MMU::Flags::SYS);
     cout << "Data base=" << *data_base << endl;
-    cout << "code seg=" << reinterpret_cast<unsigned*>(code_seg1) << endl;
-    cout << "data seg=" << reinterpret_cast<unsigned*>(data_seg1) << endl;
+    cout << "Code seg=" << reinterpret_cast<unsigned long*>(code_seg1) << endl;
 
     return 0;
 }
